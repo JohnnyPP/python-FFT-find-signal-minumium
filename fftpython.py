9332a6bc0f6fe1b7aba9f183a72e4af509857bf8
JohnnyPP <johnnnyppp@gmail.com>
2014-04-14 23:30:06 +0200

    Added finding of the maximum

--- fftpython.py
+++ fftpython.py
@@ -2,28 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-phase=170
+phase=100
 numberOfPoints=360
 phaseRAD=np.deg2rad(phase)
 
 #phase=90 gives for fft(sin(x+phaseRAD) fftPhaseHarmonic[1]=0
 #the minimum is in 180 degrees
 
-#phase=0 minimum is in 270 degrees fftPhaseHarmonic[1]=-90
-#phase=10 minimum is in 260 degrees fftPhaseHarmonic[1]=-80
+#phase=0 minimum is in 270 degrees fftPhaseHarmonic[1]=-90 max is 90 abs(fftPH[1])
+#phase=10 minimum is in 260 degrees fftPhaseHarmonic[1]=-80 max is 80
 #phase=20 minimum is in 250 degrees fftPhaseHarmonic[1]=-70
 #phase=30 minimum is in 240 degrees fftPhaseHarmonic[1]=-60
 #phase=40 minimum is in 230 degrees fftPhaseHarmonic[1]=-50
 #phase=50 minimum is in 220 degrees fftPhaseHarmonic[1]=-40
-#phase=60 minimum is in 210 degrees fftPhaseHarmonic[1]=-30
+#phase=60 minimum is in 210 degrees fftPhaseHarmonic[1]=-30 max is 30
 #phase=70 minimum is in 200 degrees fftPhaseHarmonic[1]=-20
 #phase=80 minimum is in 190 degrees fftPhaseHarmonic[1]=-10
-#phase=90 minimum is in 180 degrees fftPhaseHarmonic[1]=0
-#phase=100 minimum is in 170 degrees fftPhaseHarmonic[1]=10
+#phase=90 minimum is in 180 degrees fftPhaseHarmonic[1]=0   max is 0
+#phase=100 minimum is in 170 degrees fftPhaseHarmonic[1]=10 max 360-10
 #phase=110 minimum is in 160 degrees fftPhaseHarmonic[1]=20
 #phase=120 minimum is in 150 degrees fftPhaseHarmonic[1]=30
 #phase=130 minimum is in 140 degrees fftPhaseHarmonic[1]=40
-#phase=140 minimum is in 130 degrees fftPhaseHarmonic[1]=50
+#phase=140 minimum is in 130 degrees fftPhaseHarmonic[1]=50 max 360 - 50
 #phase=150 minimum is in 120 degrees fftPhaseHarmonic[1]=60
 #phase=160 minimum is in 110 degrees fftPhaseHarmonic[1]=70
 #phase=170 minimum is in 100 degrees fftPhaseHarmonic[1]=80 
@@ -35,8 +35,8 @@ phaseRAD=np.deg2rad(phase)
 #phase=230 minimum is in 40 degrees fftPhaseHarmonic[1]=140
 #phase=240 minimum is in 30 degrees fftPhaseHarmonic[1]=150
 #phase=250 minimum is in 20 degrees fftPhaseHarmonic[1]=160
-#phase=260 minimum is in 10 degrees fftPhaseHarmonic[1]=170
-#phase=270 minimum is in 0 degrees fftPhaseHarmonic[1]=-180
+#phase=260 minimum is in 10 degrees fftPhaseHarmonic[1]=170 max 360 - 170
+#phase=270 minimum is in 0 degrees fftPhaseHarmonic[1]=-180 max 180
 #phase=280 minimum is in 350 degrees fftPhaseHarmonic[1]=-170
 #phase=290 minimum is in 340 degrees fftPhaseHarmonic[1]=-160
 #phase=300 minimum is in 330 degrees fftPhaseHarmonic[1]=-150
@@ -65,6 +65,19 @@ def FindMinimumPhase(FFT):
         
     print minPhase
     return minPhase
+
+def FindMaximumPhase(FFT):
+    """
+    calculates the minimum phase for given angle obtained with
+    the FFT algorithm.
+    """
+    if FFT <= 0:
+        maxPhase=abs(FFT)
+    else:
+        maxPhase=360-FFT
+    
+    print maxPhase
+    return maxPhase
 
 x = np.linspace(0, 2*math.pi,numberOfPoints)   #create 360 angle points
 xFFT = np.linspace(0,numberOfPoints/2+1,numberOfPoints/2+1)   
@@ -109,28 +122,28 @@ amplitudeScaledRMSFFT = amplitudeFFT/float(dataLenghtFFT)/math.sqrt(2)
 ##############################################################################
 # Plot the results
 ##############################################################################
-plt.figure("FFT amplitude and phase coefficients")
-plt.subplot(2,1,1)
-plt.vlines(xFFT,0,amplitudeScaledFFT)
-plt.title("FFT amplitude coefficients")
-plt.xlabel("Harmonics")
-plt.ylabel("Amplitude [V]")
-plt.xlim(0,numberOfPoints/2+1) #adjuts the x axis to maximum of numberOfPoints
-plt.grid(True)
+#plt.figure("FFT amplitude and phase coefficients")
+#plt.subplot(2,1,1)
+#plt.vlines(xFFT,0,amplitudeScaledFFT)
+#plt.title("FFT amplitude coefficients")
+#plt.xlabel("Harmonics")
+#plt.ylabel("Amplitude [V]")
+#plt.xlim(0,numberOfPoints/2+1) #adjuts the x axis to maximum of numberOfPoints
+#plt.grid(True)
+#
+#plt.subplot(2,1,2)
+#plt.vlines(xFFT,0,phaseDegreesFFT)
+#plt.title("FFT phase coefficients")
+#plt.xlabel("Harmonics")
+#plt.ylabel("Phase [deg]")
+#plt.tight_layout()      #removes the overlapping of the labels in subplots
+#plt.xlim(0,numberOfPoints/2+1)
+#plt.grid(True)
 
-plt.subplot(2,1,2)
-plt.vlines(xFFT,0,phaseDegreesFFT)
-plt.title("FFT phase coefficients")
-plt.xlabel("Harmonics")
-plt.ylabel("Phase [deg]")
-plt.tight_layout()      #removes the overlapping of the labels in subplots
-plt.xlim(0,numberOfPoints/2+1)
-plt.grid(True)
-
-plt.figure("Signal with fit and found minimum")
-line, = plt.plot(np.rad2deg(x), y, linewidth=2)                 #fit
-line, = plt.plot(np.rad2deg(x), ynoise, 'ro', linewidth=3)      #signal
-plt.title("Signal with fit and found minimum")
+plt.figure("Signal with fit and found min/max")
+p1, = plt.plot(np.rad2deg(x), amplitudeScaledFFT[1]*y, linewidth=2)   #fit
+p2, = plt.plot(np.rad2deg(x), ynoise, 'ro', linewidth=3)              #signal
+plt.title("Signal with fit and found min/max")
 plt.xlabel("Angle [deg]")
 plt.ylabel("Amplitude [V]")
 plt.xlim(0,numberOfPoints)
@@ -138,5 +151,10 @@ plt.vlines(FindMinimumPhase(phaseDegreesFFT[1]),
            -amplitudeScaledFFT[1], amplitudeScaledFFT[1],linewidth=3,
             color='k',linestyles='dashed') 
             #draws the vertical line with the found minimum
+plt.vlines(FindMaximumPhase(phaseDegreesFFT[1]), 
+           -amplitudeScaledFFT[1], amplitudeScaledFFT[1],linewidth=3,
+            color='g',linestyles='dashed') 
+            #draws the vertical line with the found maximum
+plt.legend([p1, p2], ["Fit", "Signal"])
 plt.grid(True)
 plt.show()
